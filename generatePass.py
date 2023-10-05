91989d132437bb91522d03259a63baa5be615176
LowkeySnoopy <117758065+LowkeySnoopy@users.noreply.github.com>
2023-10-05 14:13:16 -0500

    Update generatePass.py

--- generatePass.py
+++ generatePass.py
@@ -1,1 +1,25 @@
+import random
+import string
 
+
+def get_random_password():
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    # select 1 lowercase
+    password = random.choice(string.ascii_lowercase)
+    # select 1 uppercase
+    password += random.choice(string.ascii_uppercase)
+    # select 1 digit
+    password += random.choice(string.digits)
+    # select 1 special symbol
+  
+   # generate other characters
+    for _i in range(4):
+        password += random.choice(random_source)
+
+    password_list = list(password)
+    # shuffle all characters
+    random.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
+    return password
+  
+print("Password:", get_random_password())
